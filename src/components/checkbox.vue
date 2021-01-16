a4bf3c269419d5f18f4865c1309163f69ca06c03
duqingbing <duqingbing@huoyunren.com>
2021-01-16 11:48:56 +0800

    添加checkout

--- src/components/checkbox.vue
+++ src/components/checkbox.vue
@@ -1,9 +1,19 @@
 <template>
-  <el-form-item :label="item.label" :key="uniqid" class="text-left">
+  <!-- 表单显示 -->
+  <el-form-item
+    v-if="isText"
+    :label="item.label"
+    :key="uniqid"
+    :style="{ textAlign: labelAlgin }"
+  >
+    {{ viewText }}
+  </el-form-item>
+
+  <!-- 表单组件 -->
+  <el-form-item v-else :label="item.label" :key="uniqid" class="text-left">
     <el-checkbox-group v-model="formData[field]">
       <template v-for="(val, key) in item.options">
-        {{ val }}
-        <el-checkbox :label="key" :key="uniqid + key" :disabled="isText" border>{{
+        <el-checkbox :label="key" :key="uniqid + key" border>{{
           val
         }}</el-checkbox>
       </template>
@@ -14,8 +24,27 @@
 <script>
 // 导入
 import Base from "./base";
+import { isArray } from "@qingbing/helper";
+
 // 导出
 export default {
   extends: Base,
+  created() {
+    if (this.isText) {
+      const selected = this.formData[this.field];
+      if (!isArray(selected)) {
+        this.viewText = "";
+        return;
+      }
+      const ts = [];
+      const options = this.item.options;
+      for (const i in selected) {
+        if (options[selected[i]]) {
+          ts.push(options[selected[i]]);
+        }
+      }
+      this.viewText = ts.join(",");
+    }
+  },
 };
 </script>
