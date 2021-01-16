1239466e43365b2d058e229c473451d3f30e1ca2
duqingbing <duqingbing@huoyunren.com>
2021-01-16 11:38:15 +0800

    修改radio和switch

--- src/components/radio.vue
+++ src/components/radio.vue
@@ -1,11 +1,21 @@
 <template>
-  <el-form-item :label="item.label" :key="uniqid" class="text-left">
+  <!-- 表单显示 -->
+  <el-form-item
+    v-if="isText"
+    :label="item.label"
+    :key="uniqid"
+    :style="{ textAlign: labelAlgin }"
+  >
+    {{ viewText }}
+  </el-form-item>
+
+  <!-- 表单组件 -->
+  <el-form-item v-else :label="item.label" :key="uniqid" class="text-left">
     <el-radio-group v-model="formData[field]">
       <template v-for="(val, key) in item.options">
-        {{ val }}
-        <el-radio :label="key" :key="uniqid + key" :disabled="isText" border>{{
-          val
-        }}</el-radio>
+        <el-radio :label="key" :key="uniqid + key" :disabled="isText" border>
+          {{ val }}
+        </el-radio>
       </template>
     </el-radio-group>
   </el-form-item>
@@ -14,8 +24,12 @@
 <script>
 // 导入
 import Base from "./base";
+import { col_value } from "@qingbing/helper";
 // 导出
 export default {
   extends: Base,
+  created() {
+    this.viewText = col_value(this.formData[this.field], this.item.options, "");
+  },
 };
 </script>
--- src/components/switch.vue
+++ src/components/switch.vue
@@ -1,8 +1,19 @@
 <template>
-  <el-form-item :label="item.label" :key="uniqid" class="text-left">
+  <!-- 表单显示 -->
+  <el-form-item
+    v-if="type == 'text'"
+    :label="item.label"
+    :key="uniqid"
+    :style="{ textAlign: labelAlgin }"
+  >
+    {{ viewText }}
+  </el-form-item>
+
+  <!-- 表单组件 -->
+  <el-form-item v-else :label="item.label" :key="uniqid" class="text-left">
     <el-switch
       v-model="formData[field]"
-      :disabled="isText"
+      :disabled="type == 'view'"
       :activeText="activeText"
       :activeValue="activeValue"
       :inactiveText="inactiveText"
@@ -20,6 +31,7 @@ import { isArray, isObject } from "@qingbing/helper";
 export default {
   extends: Base,
   created() {
+    // 计算 activeText、inactiveText、activeValue、inactiveValue
     this.activeText = "";
     this.inactiveText = "";
     if (isArray(this.item.options)) {
@@ -34,6 +46,19 @@ export default {
     } else {
       this.activeValue = "1";
       this.inactiveValue = "0";
+    }
+    // 计算 type、 viewText
+    if (!this.isText) {
+      this.type = "form";
+    } else if (this.activeText && this.inactiveText) {
+      this.type = "text";
+      if ((this.formData[this.field], this.activeValue)) {
+        this.viewText = this.activeText;
+      } else {
+        this.viewText = this.inactiveText;
+      }
+    } else {
+      this.type = "view";
     }
   },
 };
